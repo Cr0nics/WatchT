c4e28be8d8416dd923ea1e55fd93dec111cbfd00
User <>
2023-07-10 10:30:53 -0400

    FULL MENU LOGIC + ALL BUGS FIXED

--- app/src/main/java/com/example/watcht/ui/MainActivity.kt
+++ app/src/main/java/com/example/watcht/ui/MainActivity.kt
@@ -9,11 +9,9 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.example.watcht.R
+import com.example.watcht.databinding.ActivityMainBinding
 import com.example.watcht.ui.view.PopularMovies.MovieListFragment
 import com.example.watcht.ui.view.menuDetails.about.AboutMeFragment
 import com.example.watcht.ui.view.menuDetails.contact.ContactMeFragment
@@ -24,7 +22,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -39,12 +37,22 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.nav_view)
 
-        navController = findNavController(R.id.fragment_container)
-        setupActionBarWithNavController(navController, drawerLayout)
-        navigationView.setupWithNavController(navController)
-
+        navController = this.findNavController(R.id.fragment_container)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.movieListFragment,
+                R.id.settingsFragment,
+                R.id.contactMeFragment,
+                R.id.aboutMeFragment
+            )
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, navController)
     }
 
-    //ESTA ES LA FUNCION
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.fragment_container)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
 
 }
